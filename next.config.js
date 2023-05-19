e0af810f3058e457e44be13f270b9e7f5cbd888e
aziz_dhaifullah <aziz@investree.id>
2023-05-19 16:07:53 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    loaderFile: '/',
+  },
   unoptimized: true
 }
 
